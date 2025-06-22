a7eda6e2599e7f7403188b2cf51e101bc54a6677
areviik <vardanyanarev48@gmail.com>
2025-06-22 21:30:43 +0400

    add validation and sort

---
+++ main.c
@@ -0,0 +1,19 @@
+#include "push_swap.h"
+
+int main(int argc, char **argv)
+{
+    t_stack a;
+    t_stack b;
+
+    if (argc < 2)
+    {
+        write(2, "Error\n", 6);  // Printing error can be made as a seperate function
+        exit(EXIT_FAILURE);      // 1 line instead of 4
+    }
+    a = NULL;
+    b = NULL;
+    /*
+        MUST BE: Validation check and Sort check (if not sorted SORT);
+    */
+   return (0);
+}
