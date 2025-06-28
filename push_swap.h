2bf03c8c788c238bc76be601276d5a1d6cd13620
areviik <vardanyanarev48@gmail.com>
2025-06-28 15:56:35 +0400

    bringing me down into the ground

--- push_swap.h
+++ push_swap.h
@@ -18,5 +18,12 @@ typedef struct s_stack
 void sa(t_stack **a);
 void sb(t_stack **b);
 void ss(t_stack **a, t_stack **b);
+void push_front(t_stack **lst, int val, int ind);
+void push_down(t_stack **lst, int val, int ind); 
+void pop_front(t_stack **lst);
+void pop_down(t_stack **lst); 
+void pa(t_stack **a, t_stack **b);
+void pb(t_stack **a, t_stack **);
+int stack_size(t_list *a);
 
 #endif
